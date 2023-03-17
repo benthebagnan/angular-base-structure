18428a8ec6ba7941114dd20e2a42cb83cabac027
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-03-17 09:17:13 +0000

    Add routerModule

--- src/app/core/services/form/form.service.ts
+++ src/app/core/services/form/form.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, delay, mapTo, Observable, of } from 'rxjs';
+import { ComplexFormValue } from 'src/app/models/complex-form.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  saveUserInfo(formValue: ComplexFormValue): Observable<boolean> {
+    return this.http
+      .post('https://jsonplaceholder.typicode.com/users', formValue)
+      .pipe(
+        mapTo(true),
+        delay(1000),
+        catchError(() => of(false).pipe(delay(1000)))
+      );
+  }
 }
